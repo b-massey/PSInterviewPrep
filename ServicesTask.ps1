bbcf1a5c87cac70f55b96995c3b5cb15f4977cc8
bm-2017 <bindu.massey@hotmail.co.uk>
2017-02-12 11:34:47 +0000

    Add new file ServicesTask.ps1 for Scenario 1

---
+++ ServicesTask.ps1
@@ -0,0 +1,16 @@
+﻿Function ListRunningServices() {
+    $ComputerName = "$env:COMPUTERNAME"
+    $ServiceState = "running"
+    $csvFileName = "servicesList.csv"
+
+    Get-WmiObject -Class Win32_Service -ComputerName $ComputerName | 
+        Where-Object State -EQ $ServiceState | 
+        Select-Object DisplayName, StartName, State |
+        Export-Csv $csvFileName -NoTypeInformation
+
+
+
+}
+
+
+ListRunningServices
